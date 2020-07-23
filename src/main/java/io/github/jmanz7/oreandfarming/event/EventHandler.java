aadc0c0a38896546e96c2be28ec78c0699784d89
MaroonShaded <maroonshaded@gmail.com>
2020-07-23 21:51:37 +0100

    Add green apple to farmer villager trades

--- src/main/java/io/github/jmanz7/oreandfarming/event/EventHandler.java
+++ src/main/java/io/github/jmanz7/oreandfarming/event/EventHandler.java
@@ -1,10 +1,16 @@
 package io.github.jmanz7.oreandfarming.event;
 
+import io.github.jmanz7.oreandfarming.init.ModItems;
 import net.minecraft.block.*;
 import net.minecraft.entity.item.ExperienceOrbEntity;
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraft.entity.merchant.villager.VillagerTrades;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.BasicTrade;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -33,5 +39,15 @@ public class EventHandler
 
 		if (!world.isRemote && !event.getPlayer().abilities.isCreativeMode && state.getBlock() instanceof SweetBerryBushBlock && state.get(SweetBerryBushBlock.AGE) == 3)
 			world.addEntity(new ExperienceOrbEntity(world, (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, ExperienceOrbEntity.getXPSplit(1)));
+	}
+
+	@SubscribeEvent
+	public static void registerTrades(VillagerTradesEvent event)
+	{
+		if (event.getType() == VillagerProfession.FARMER)
+		{
+			VillagerTrades.ITrade trade = new BasicTrade(1, new ItemStack(ModItems.GREEN_APPLE.get()), 16, 5, 0.05F);
+			event.getTrades().get(2).add(trade);
+		}
 	}
 }
